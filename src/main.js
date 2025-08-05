609beb8be47ce817856682632cc6d906d4910536
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-08-05 15:32:29 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 
 import * as STORAGE from './lib/store-cfg.js';
 import * as CONNSDATA from './lib/conns-data.js';
-import * as MQTT from './src/lib/mqtt.js';
-import * as UI from './src/ui-core.js';
+import * as MQTT from './lib/mqtt.js';
+import * as UI from './ui-core.js';
 
 STORAGE.loadConfig();
 MQTT.connectToFeed();
