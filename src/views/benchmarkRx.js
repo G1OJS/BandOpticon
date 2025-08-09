7015db25e8f464fd79e8e939ce9c42d6227786ac
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-08-09 18:08:51 +0100

    Add components to read all.txt

--- src/views/benchmarkRx.js
+++ src/views/benchmarkRx.js
@@ -3,6 +3,7 @@ import * as CONNSDATA from '../lib/conns-data.js';
 import * as STORAGE from '../lib/store-cfg.js';
 import {squareIsInHome} from '../lib/geo.js';
 
+
 var activeModes = new Set(); // updated to be relevant to the current view and then passed back to ribbon
 let registerActiveModes = () => {};  // fallback to no-op
 
@@ -21,18 +22,32 @@ export function init(container, opts = {}) {
   if (opts.registerActiveModes) {
     registerActiveModes = opts.registerActiveModes;
   }
-  DOMcontainer.addEventListener('click', (e) => {
-	  const id = e.target.id;
-	  let grp = id.split('.')[0];
-	  let val = id.split('.')[1];
-	  if (val && "CSL2L4L6".includes(val)) {
-		if(grp == "Home_Entity_Type"){entityTypeHome = val;}
-		if(grp == "Remote_Entity_Type"){entityTypeRemote = val;}
-		refresh();
-	  }
-  });
-
+  refresh();
 }
+
+  function handleFiles() {
+    const fileList = document.getElementById('allFileChooser').files;
+	console.log(fileList[0]);
+	const reader = new FileReader();
+	reader.onload = () => {
+		console.log(reader.result);
+		// message format:
+		// sq:sequence number b:band f:frequency md:mode rp:report (snr) t:seconds since 1970-01-01
+		// sc/rc:sender call/receiver call sl/rl:sender locator/receiver locator sa/ra:sender ADIF/receiver ADIF
+		// first, build spot object for this msg using same keys as PSKR MQTT:
+		
+		
+		
+		//addSpotToConnectivityMap(spot);
+	};
+	reader.onerror = () => {
+		showMessage("Error reading the file. Please try again.", "error");
+	};
+	reader.readAsText(fileList[0]);
+  }
+
+
+
 
 export function refresh(){
 	// Update activeModes for all modes found on this band ONLY
@@ -47,13 +62,18 @@ export function refresh(){
 	registerActiveModes(activeModes);	
 	mode = getMode();
 	console.log("Rx Benchmarking for ", mode);
-	let HTML = '<h2>Rx Benchmarking for ' + mode +'</h2>';
+	let HTML = ""
+	HTML +=  '<h2>Rx Benchmarking for ' + mode +'</h2>';
 	HTML += "<div class = 'text-sm'>";
-	HTML += "This is a new view showing the number of times a pskreporter report was made for each transmitting callsign."
+	HTML += "This is a new view showing the number of times a pskreporter report was made for each transmitting callsign. "
 	HTML += "Note that pskreporter only re-issues a report for a spot if 20 minutes have elapesed from the previous spot."
+	HTML += '<br><label>Import ALL.txt </label><input type="file" class = "button" id="allFileChooser" accept="*.txt" />'
 	HTML += "</div><br>";
 	HTML += html_for_benchmarking(mode);
 	DOMcontainer.innerHTML = HTML;
+	
+	const inputElement = document.getElementById('allFileChooser');
+    inputElement.addEventListener("change", handleFiles, false);
 }
 
 function html_for_benchmarking(mode){
