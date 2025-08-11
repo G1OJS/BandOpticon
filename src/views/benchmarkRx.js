e9887cacac63d6f70f1929ec73e82e47fcb57095
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-08-11 22:46:19 +0100

    Update benchmarkRx.js

--- src/views/benchmarkRx.js
+++ src/views/benchmarkRx.js
@@ -24,6 +24,7 @@ export function refresh(){
 	HTML += "This is a new view (still being developed) showing the latest report from each of 'My Callsigns' for each transmitting callsign heard. "
 	HTML += "Note that pskreporter only re-issues a report for a spot if 20 minutes have elapesed from the previous spot. "
 	HTML += "The view allows comparison of Rx performance with other callsigns, or between multiple receive configuratons providing seperate reports to pskreporter."
+	HTML += "<strong>To see comparisons, enter additional callsigns in the 'My Callsign(s)' box above - either another station's, or your callsign for a secondary Rx.</strong>"
 	HTML += "</div><br>";
 	
 	HTML += "<canvas id='benchmarkGraph' style='width:100%;max-width:700px'></canvas>"
@@ -32,6 +33,7 @@ export function refresh(){
 		HTML += "<li>Integrate mode buttons</li>";
 		HTML += "<li>Show band information</li>";
 		HTML += "<li>Show range of SNR not just max</li>";
+		HTML += "<li>Add distance / bearing / options</li>";
 		HTML += "</ul>";
 	
 	DOMcontainer.innerHTML = HTML;
