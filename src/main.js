6a068de1fa853a4500e2c9a1df81c0cd29bcd875
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-09-08 22:54:43 +0100

    Update details plot

--- src/main.js
+++ src/main.js
@@ -23,21 +23,16 @@ setInterval(() => purgeLiveConnections(), 5000);
 setInterval(() => ribbon.writeModeButtons(), 5000);
 setInterval(() => refreshMainView(), 5000);
 
-const coloursList = ["#25CCF7","#FD7272","#54a0ff","#00d2d3","#1abc9c","#2ecc71","#3498db","#9b59b6","#34495e","#16a085","#27ae60","#2980b9","#8e44ad","#2c3e50",
-					 "#f1c40f","#e67e22","#e74c3c","#ecf0f1","#95a5a6","#f39c12","#d35400","#c0392b","#bdc3c7","#7f8c8d","#55efc4","#81ecec","#74b9ff","#a29bfe","#dfe6e9",
-					 "#00b894","#00cec9","#0984e3","#6c5ce7","#ffeaa7","#fab1a0","#ff7675","#fd79a8","#fdcb6e","#e17055","#d63031","#feca57","#5f27cd","#54a0ff","#01a3a4"]
-const coloursForAggregates =   {myCall_tx:'rgba(255, 20, 20, 1)', leader_tx:'rgba(255, 99, 132, 0.4)', all_tx:'rgba(255, 99, 132, 0.1)',
-								myCall_rx:'rgba(20, 20, 200, 1)', leader_rx:'rgba(54, 162, 200, 0.4)',all_rx:'rgba(54, 162, 200, 0.1)'};
+const coloursForAggregates =   {myCall_tx:'rgba(255, 20, 20, 1)', all_tx:'rgba(255, 99, 132, 0.1)',
+								myCall_rx:'rgba(20, 20, 200, 1)', all_rx:'rgba(54, 162, 200, 0.1)'};
 
 html ="";
 html +="<div class = 'legendItem'><b>Receiver:</b> </div>";
 html +="<div class = 'legendItem'><span class = 'legendMarker' style='background:" +  coloursForAggregates.myCall_rx + "'></span>"+myCall+"</div>";
-html +="<div class = 'legendItem'><span class = 'legendMarker' style='background:" +  coloursForAggregates.leader_rx + "'></span>Band leader</div>";
 html +="<div class = 'legendItem'><span class = 'legendMarker' style='background:" +  coloursForAggregates.all_rx + "'></span>All home  </div>";
 html +="<div class = 'legendItem' style='width:50px;'>&nbsp </div>";
 html +="<div class = 'legendItem'><b>Transmitter:</b> </div>";
 html +="<div class = 'legendItem'><span class = 'legendMarker' style='background:" +  coloursForAggregates.myCall_tx + "'></span>"+myCall+"</div>";
-html +="<div class = 'legendItem'><span class = 'legendMarker' style='background:" +  coloursForAggregates.leader_tx + "'></span>Band leader</div>";
 html +="<div class = 'legendItem'><span class = 'legendMarker' style='background:" +  coloursForAggregates.all_tx + "'></span>All home</div>";
 html +="</div>";
 let overviewLegendHTML = html;
@@ -136,17 +131,9 @@ function drawBandTile(canvas_id, title_el, band){
 			if(rc == myCall) check_add(sc, hearingMe.data); 
 		}
 	}
-
 	const data = { datasets: [	hearingHome, heardbyHome, hearingMe, heardbyMe ]};
 	
 	if(charts[canvas_id]?.['chart']) {charts[canvas_id]['chart'].destroy()}
-	
-	let scales ={};
-	scales = {
-		x: {display:false, title: {display:false, text: 'Longitude'}, type: 'linear',position: 'bottom' , max:180, min:-180},
-		y: {display:false, title: {display:false, text: 'Lattitude'}, type: 'linear',position: 'left', max:90, min: -90}
-	};
-
 	charts[canvas_id]={};
 	charts[canvas_id]['chart'] = new Chart(
 		document.getElementById(canvas_id),
@@ -156,13 +143,15 @@ function drawBandTile(canvas_id, title_el, band){
 				tooltip:{callbacks: {label: function(context) {let label = context.dataset.label || ''; return label;} }},
 				legend: {display:false},             
 						title: {display: false, align:'start', text: " "+band}},
-			scales: scales
+			scales: {
+				x: {display:false, title: {display:false, text: 'Longitude'}, type: 'linear',position: 'bottom' , max:180, min:-180},
+				y: {display:false, title: {display:false, text: 'Lattitude'}, type: 'linear',position: 'left', max:90, min: -90}
+				}
 			}
 		}
 	);	
 	
 	charts[canvas_id]['band']=band;
-
 }
 
 function hex2rgba(hex){
@@ -174,74 +163,34 @@ function hex2rgba(hex){
 }
 
 function drawSingle(){
-
 	let band = bands[0];
+	let conns = connsData[band][mode];
 	let canvas_id = "bandTileCanvas";
 	document.getElementById('bandTile').classList.remove("hidden");
 	document.getElementById(canvas_id).classList.remove("hidden");
 	
-	let rx_conns_data = liveConnsData[band]?.[mode]?.['Rx'];
-	let tx_conns_data = liveConnsData[band]?.[mode]?.['Tx'];
-	let rx_aggs =getAggregatedCallLists(rx_conns_data);
-	let tx_aggs =getAggregatedCallLists(tx_conns_data);
-	
-	let callsignColours = {};
-	let colIdx = 0;
-	for (const hc in rx_conns_data){
-		if(!(hc in callsignColours)) {colIdx += 1}
-		callsignColours[hc] = hex2rgba(coloursList[colIdx]);
-	}
-	for (const hc in tx_conns_data){
-		if(!(hc in callsignColours)) {colIdx += 1}
-		callsignColours[hc] = hex2rgba(coloursList[colIdx]);
-	}
-	
-	let datasets = [];
-	for (const hc in rx_conns_data){ 
-		let ocLocs = [];
-		let hcLoc = call_locs[hc];
-		let hcLines = []
-		let oCalls =[];
-		for (const oc in rx_conns_data[hc]){
-			hcLines.push(hcLoc)
-			hcLines.push(call_locs[oc])
-			ocLocs.push(call_locs[oc])
-			oCalls.push("TX:"+oc);
+	let tx_lines = [];
+	let rx_lines = [];
+	for (const sc in conns){ 
+		for (const rc in conns[sc]){
+			if(callsData[sc].inHome){
+				tx_lines.push({x:callsData[sc].x, y:callsData[sc].y})
+				tx_lines.push({x:callsData[rc].x, y:callsData[rc].y})
+			}
+			if(callsData[rc].inHome){
+				rx_lines.push({x:callsData[rc].x, y:callsData[rc].y})
+				rx_lines.push({x:callsData[sc].x, y:callsData[sc].y})
+			}
 		}
-		datasets.push({type: 'scatter', label: '', data: hcLines, showLine: true, pointRadius:0,  borderColor: callsignColours[hc] });
-        datasets.push({type: 'scatter', label: 'tx', data: ocLocs, pointRadius:4, backgroundColor:'rgba(255,0,0,0.6)'});
-        datasets.push({type: 'scatter', label: hc+'(rx)', data: [hcLoc], pointRadius:7, backgroundColor:'rgba(0,0,355,0.3)'});
-	}
-	for (const hc in tx_conns_data){ 
-		let ocLocs = [];
-		let hcLoc = call_locs[hc];
-		let hcLines = [];
-		let oCalls =[];
-		for (const oc in tx_conns_data[hc]){
-			hcLines.push(hcLoc)
-			hcLines.push(call_locs[oc])
-			ocLocs.push(call_locs[oc])
-			oCalls.push("RX:"+oc);
-		}
-		datasets.push({type: 'scatter', label: '', data: hcLines, showLine: true, pointRadius:0, borderColor: callsignColours[hc] });
-        datasets.push({type: 'scatter', label: hc+'(tx)', data: [hcLoc], pointRadius:4, backgroundColor:'rgba(255,0,0,0.6)'});
-        datasets.push({type: 'scatter', label: 'rx', data: ocLocs, pointRadius:7, backgroundColor:'rgba(0,0,355,0.3)'});
 	}
 
-	let data = {datasets};
-	
-	document.getElementById('bandTileTitle').innerHTML = prettyInfo(band, rx_aggs, tx_aggs)
+	let data = { datasets: [
+						{type: 'scatter', label: 'Tx', data: tx_lines, showLine: true, pointRadius:0, borderColor: coloursForAggregates.all_tx},
+						{type: 'scatter', label: 'Rx', data: rx_lines, showLine: true, pointRadius:0, borderColor: coloursForAggregates.all_rx }
+					]
+				};
 
-	let scales ={};
-	scales = {
-		x: {display:true, title: {display:true, text: 'Longitude'}, type: 'linear',position: 'bottom'},
-		y: {display:true, title: {display:true, text: 'Lattitude'}, type: 'linear',position: 'left'}
-	};
-	
-	if(charts[canvas_id]?.['chart']){
-		charts[canvas_id]['chart'].destroy()
-	}
-
+	if(charts[canvas_id]?.['chart']){charts[canvas_id]['chart'].destroy()}
 	charts[canvas_id]={};
 	charts[canvas_id]['chart'] = new Chart(
 		document.getElementById(canvas_id),
@@ -251,7 +200,10 @@ function drawSingle(){
 						tooltip:{callbacks: {label: function(context) {let label = context.dataset.label || ''; return label;} }},
 						legend: {display: false},             
 						title: {display: false, align:'start', text: " "}},
-			scales: scales
+			scales: {
+				x: {display:true, title: {display:true, text: 'Longitude'}, type: 'linear',position: 'bottom'},
+				y: {display:true, title: {display:true, text: 'Lattitude'}, type: 'linear',position: 'left'}
+				}
 			}
 		}
 	);	
